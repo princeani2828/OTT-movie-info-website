dabfcc01d26572412522dd3d033b0f8ff0d10b43
PRINCE <146663532+princeani2828@users.noreply.github.com>
2025-01-26 01:27:47 +0530

    Add files via upload

---
+++ PROJECT/html/one.js
@@ -0,0 +1,23 @@
+
+
+   
+
+function lifeInWeeks(age) {
+
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    
+
+    let val=input*365;
+    let one=32850-val;
+    let two=one/365;
+    console.log(two);
+    
+    
+    
+/*************Don't change the code below**********/
+}
+
+let input = prompt("Enter the value:");
+lifeInWeeks(input);
